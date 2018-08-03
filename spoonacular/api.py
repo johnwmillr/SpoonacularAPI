d620aee2098bf28e7f323d064d0d14bf9183a488
johnwmillr <john.w.millr@gmail.com>
2018-08-03 00:49:19 -0500

    Add detect_food_in_text endpoint

--- spoonacular/api.py
+++ spoonacular/api.py
@@ -16,7 +16,9 @@ class API(object):
 
     # Create a persistent requests connection
     session = requests.Session()
-    session.headers = {"Application": "PySpoon","Content-Type": "application/x-www-form-urlencoded","Accept": "application/json"}
+    session.headers = {"Application": "PySpoon",
+                       "Content-Type": "application/x-www-form-urlencoded",
+                       "Accept": "application/json"}
 
     def __init__(self, api_key, format_='json', timeout=5, sleep_time=1.5):
         """ Spoonacular API Constructor
@@ -37,12 +39,8 @@ class API(object):
         """Make a request to the API"""
 
         uri = self.api_root + path
-        print(uri)
-        print(query_)
-        print(params_)
-        print(self.session.headers)
         try:
-            response = self.session.request(method.upper(), uri,
+            response = self.session.request(method, uri,
                                             timeout=self.timeout,
                                             data=query_,
                                             params=params_)
@@ -53,7 +51,18 @@ class API(object):
         time.sleep(self.sleep_time)
         return response
 
+    def detect_food_in_text(self, text):
+        """ Detect ingredients and dishes in texts
+            https://market.mashape.com/spoonacular/recipe-food-nutrition#detect-food-in-text
+        """
+        endpoint = "food/detect"
+        query = {"text": text}
+        return self._make_request(endpoint, method='Post', query_=query)
+
     def parse_ingredients(self, ingredientList, servings=1, includeNutrition=False):
+        """ Extract an ingredient from plain text
+            https://market.mashape.com/spoonacular/recipe-food-nutrition#parse-ingredients
+        """
         endpoint = "recipes/parseIngredients"
         query = {"ingredientList": ingredientList, "servings": servings}
         params = {"includeNutrition": includeNutrition}
