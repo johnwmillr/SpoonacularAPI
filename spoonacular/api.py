0c3127d71aa7570f998aa33df08cb7a1dace4983
johnwmillr <john.w.millr@gmail.com>
2018-08-05 01:17:24 -0500

    Add support for the Search endpoints

--- spoonacular/api.py
+++ spoonacular/api.py
@@ -59,6 +59,8 @@ class API(object):
         time.sleep(self.sleep_time)
         return response
 
+    """ EXTRACT Endpoints """
+
     def analyze_a_recipe_search_query(self, q):
         """ Parse a recipe search query to find out its intention.
             https://market.mashape.com/spoonacular/recipe-food-nutrition#analyze-a-recipe-search-query
@@ -103,3 +105,137 @@ class API(object):
         query = {"ingredientList": ingredientList, "servings": servings}
         params = {"includeNutrition": includeNutrition}
         return self._make_request(endpoint, method="POST", query_=query, params_=params)
+
+    """ SEARCH Endpoints """
+
+    def autocomplete_ingredient_search(self, query, intolerances=None, metaInformation=None, number=None):
+        """ Autocomplete a search for an ingredient.
+            https://market.mashape.com/spoonacular/recipe-food-nutrition#autocomplete-ingredient-search
+        """
+        endpoint = "food/ingredients/autocomplete"
+        query = {}
+        params = {"intolerances": intolerances, "metaInformation": metaInformation, "number": number, "query": query}
+        return self._make_request(endpoint, method="GET", query_=query, params_=params)
+
+    def autocomplete_recipe_search(self, query, number=None):
+        """ Autocomplete a partial input to possible recipe names.
+            https://market.mashape.com/spoonacular/recipe-food-nutrition#autocomplete-recipe-search
+        """
+        endpoint = "recipes/autocomplete"
+        query = {}
+        params = {"number": number, "query": query}
+        return self._make_request(endpoint, method="GET", query_=query, params_=params)
+
+    def get_comparable_products(self, upc):
+        """ Find comparable products to the given one.
+            https://market.mashape.com/spoonacular/recipe-food-nutrition#get-comparable-products
+        """
+        endpoint = "food/products/upc/{upc}/comparable"
+        query = {}
+        params = {"upc": upc}
+        return self._make_request(endpoint, method="GET", query_=query, params_=params)
+
+    def get_dish_pairing_for_wine(self, wine):
+        """ Get a dish that goes well with a given wine.
+            https://market.mashape.com/spoonacular/recipe-food-nutrition#get-dish-pairing-for-wine
+        """
+        endpoint = "food/wine/dishes"
+        query = {}
+        params = {"wine": wine}
+        return self._make_request(endpoint, method="GET", query_=query, params_=params)
+
+    def get_ingredient_substitutes(self, ingredientName):
+        """ Get ingredient substitutes by ingredient name.
+            https://market.mashape.com/spoonacular/recipe-food-nutrition#get-ingredient-substitutes
+        """
+        endpoint = "food/ingredients/substitutes"
+        query = {}
+        params = {"ingredientName": ingredientName}
+        return self._make_request(endpoint, method="GET", query_=query, params_=params)
+
+    def get_ingredient_substitutes_by_id(self, id):
+        """ Search for substitutes for a given ingredient.
+            https://market.mashape.com/spoonacular/recipe-food-nutrition#get-ingredient-substitutes-by-id
+        """
+        endpoint = "food/ingredients/{id}/substitutes"
+        query = {}
+        params = {"id": id}
+        return self._make_request(endpoint, method="GET", query_=query, params_=params)
+
+    def get_random_recipes(self, limitLicense=None, number=None, tags=None):
+        """ Find random (popular) recipes.
+            https://market.mashape.com/spoonacular/recipe-food-nutrition#get-random-recipes
+        """
+        endpoint = "recipes/random"
+        query = {}
+        params = {"limitLicense": limitLicense, "number": number, "tags": tags}
+        return self._make_request(endpoint, method="GET", query_=query, params_=params)
+
+    def get_similar_recipes(self, id):
+        """ Find recipes which are similar to the given one.
+            https://market.mashape.com/spoonacular/recipe-food-nutrition#get-similar-recipes
+        """
+        endpoint = "recipes/{id}/similar"
+        query = {}
+        params = {"id": id}
+        return self._make_request(endpoint, method="GET", query_=query, params_=params)
+
+    def get_wine_description(self, wine):
+        """ Get the description of a certain wine, e.g. "malbec" or "riesling"
+            https://market.mashape.com/spoonacular/recipe-food-nutrition#get-wine-description
+        """
+        endpoint = "food/wine/description"
+        query = {}
+        params = {"wine": wine}
+        return self._make_request(endpoint, method="GET", query_=query, params_=params)
+
+    def get_wine_pairing(self, food, maxPrice=None):
+        """ Find a wine that goes well with a food. Food can be a dish name ("steak"),
+            an ingredient name ("salmon"), or a cuisine ("italian").
+            https://market.mashape.com/spoonacular/recipe-food-nutrition#get-wine-pairing
+        """
+        endpoint = "food/wine/pairing"
+        query = {}
+        params = {"food": food, "maxPrice": maxPrice}
+        return self._make_request(endpoint, method="GET", query_=query, params_=params)
+
+    def get_wine_recommendation(self, wine, maxPrice=None, minRating=None, number=None):
+        """ Get a specific wine recommendation (concrete product) for a given wine, e.g.
+            "merlot".
+            https://market.mashape.com/spoonacular/recipe-food-nutrition#get-wine-recommendation
+        """
+        endpoint = "food/wine/recommendation"
+        query = {}
+        params = {"maxPrice": maxPrice, "minRating": minRating, "number": number, "wine": wine}
+        return self._make_request(endpoint, method="GET", query_=query, params_=params)
+
+    def search_grocery_products_by_upc(self, upc):
+        """ Get information about a food product given its UPC.
+            https://market.mashape.com/spoonacular/recipe-food-nutrition#search-grocery-products-by-upc
+        """
+        endpoint = "food/products/upc/{upc}"
+        query = {}
+        params = {"upc": upc}
+        return self._make_request(endpoint, method="GET", query_=query, params_=params)
+
+    def search_recipes_by_ingredients(self, ingredients, fillIngredients=None, limitLicense=None, number=None, ranking=None):
+        """ Find recipes that use as many of the given ingredients as possible and have
+            as little as possible missing ingredients. This is a whats in your fridge
+            API endpoint.
+            https://market.mashape.com/spoonacular/recipe-food-nutrition#search-recipes-by-ingredients
+        """
+        endpoint = "recipes/findByIngredients"
+        query = {}
+        params = {"fillIngredients": fillIngredients, "ingredients": ingredients, "limitLicense": limitLicense, "number": number, "ranking": ranking}
+        return self._make_request(endpoint, method="GET", query_=query, params_=params)
+
+    def search_site_content(self, query):
+        """ Search spoonacular's site content. You'll be able to find everything that
+            you could also find using the search suggests on spoonacular.com. This
+            is a suggest API so you can send partial strings as queries.
+            https://market.mashape.com/spoonacular/recipe-food-nutrition#search-site-content
+        """
+        endpoint = "food/site/search"
+        query = {}
+        params = {"query": query}
+        return self._make_request(endpoint, method="GET", query_=query, params_=params)
